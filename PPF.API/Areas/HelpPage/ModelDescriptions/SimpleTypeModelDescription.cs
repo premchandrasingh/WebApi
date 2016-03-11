85302b9d29343a44feba2a5be307dcb583c196e1
Prem <Premchandrasingh@gmail.com>
2016-03-11 18:24:57 +0530

    Clean Repository

--- PPF.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace PPF.API.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- PPF.API/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PPF.API.WebApiApplication" Language="C#" %>
